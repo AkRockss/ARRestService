a032068adbbd52a3ca2de1e1c46effb3e3b5412e
Aleksander Kruse Schaumburg-Müller <sanderkruse1987@gmail.com>
2023-01-13 10:12:35 +0100

    #Fix: Add to unitTest Productmanager

--- ARRestServiceTests/Managers/ProductManagerTests.cs
+++ ARRestServiceTests/Managers/ProductManagerTests.cs
@@ -29,7 +29,8 @@ namespace ARRestService.Managers.Tests
         [TestMethod()]
         public void TestGetAllProducts()
         {
-            IEnumerable<Products> products = _productManager.GetAll();
+            IEnumerable<Products> products =
+            _productManager.GetAll();
             Assert.AreEqual(3, products.Count());
         }
 
@@ -45,46 +46,28 @@ namespace ARRestService.Managers.Tests
         [TestMethod()]
         public void TestAddProduct()
         {
-            Products a = new() { productId = 4, productBrand = "NOR", productName = "Makrel", productDescription = "PD4", organic = true, noeglemaerket = true };
+            Products a = new() { productId = 4, productBrand = "NOR", productName = 
+            "Makrel", productDescription = "PD4", organic = true, noeglemaerket = true };
             _context.Add(a);
             _context.SaveChanges();
 
             Assert.AreEqual("Makrel", a.productName);
             Assert.AreEqual(4, _productManager.GetAll().Count());
 
-            //a.productName = "updatedName";
-            //Products updatedProduct = _productManager.Update(a.productId, a);
-            //Assert.AreEqual("updatedName", updatedProduct.productName);
-
-
-
-
-        //[TestMethod()]
-        //public void TestUpdateProduct()
-        //{
-        //    Products updates = new() {productId = 5, productName = "LOMO", productBrand = "SCW", productDescription = "PD5", organic = false, noeglemaerket = false };
-        //    _context.Add(updates);
-        //    _context.SaveChanges();
-        //    Products updatedProduct = _productManager.Update(5, updates);
-        //    Assert.AreEqual(5, updatedProduct.productId);
-        //    Assert.AreEqual(updates.productName, updatedProduct.productName);
-        //    Assert.AreEqual(updates.productBrand, updatedProduct.productBrand);
-        //    Assert.AreEqual(updates.productDescription, updatedProduct.productDescription);
-        //    Assert.AreEqual(updates.organic, updatedProduct.organic);
-        //    Assert.AreEqual(updates.noeglemaerket, updatedProduct.noeglemaerket);
-
-        //    Assert.IsNull(_productManager.Update(5, updates));
-
+            a.productName = "updatedName";
+            Products updatedProduct = _productManager.Update(a.productId, a);
+            Assert.AreEqual("updatedName", updatedProduct.productName);
+    
         }
-
 
 
         [TestMethod()]
         public void TestDeleteProduct()
         {
 
-            Products deleteProduct = _productManager.Delete(4);
-            Assert.AreEqual("Makrel", deleteProduct.productName);
+            Products deleteProduct = 
+            _productManager.Delete(4);
+            Assert.AreEqual("updatedName", deleteProduct.productName);
             Assert.AreEqual(3, _productManager.GetAll().Count());
 
         }
